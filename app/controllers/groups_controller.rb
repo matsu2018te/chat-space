d4ff861dd9d61b989d924f6d6a55c944c6620cd0
matsu2018te <hiroyukimatsumoto@MacBook-Air-HM.local>
2018-11-28 14:32:03 +0900

    グループ作成機能を実装する

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,17 @@
 class GroupsController < ApplicationController
 
-  def index
-  end
-
   def new
+    @group = Group.new
+    @group.users << current_user #ログイン中のユーザーインスタンスを、新規作成したグループに追加
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
