8f57b1e2a2676563d25d74c2e76bb42fb0ed5fce
matsu2018te <hiroyukimatsumoto@MacBook-Air-HM.local>
2018-12-02 00:42:46 +0900

    インクリメンタルサーチ実装

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,79 @@
 $(function() {
+  var user_search_result = $('#user-search-result');//追加変数
+  var chat_group_users = $('#chat-group-users');
+
+function appendUsers(users) {
+  var user_html = `
+                  <div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">
+                      ${ users.name }
+                    </p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ users.id }" data-user-name="${ users.name }">
+                      追加
+                    </a>
+                  </div>
+                  `
+  user_search_result.append(user_html);
+}
+
+function appendNoUsers(users) {
+  var user_html = `
+                  <div class="chat-group-user clearfix">${ users }</div>
+                  `
+  user_search_result.append(user_html)
+}
+
+function addUser(user_id, user_name) {
+  var user_html = `
+                  <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                    <input name='group[user_ids][]' type='hidden' data-user-id='${user_id}>
+                    <p class='chat-group-user__name', id = 'data-user-name'>
+                      ${user_name}
+                    </p>
+                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>
+                      削除
+                    </a>
+                  </div>
+                  `
+  chat_group_users.append(user_html);
+}
+
   $("#user-search-field").on("keyup", function() {
-    var input = $("#user-search-field").val();
-    console.log(input);
+    var input_name = $("#user-search-field").val();
+      $(user_search_result).empty();
+
+        $.ajax({
+          type: 'GET',
+          url: '/users',
+          data: { keyword: input_name },
+          dataType: 'json'
+        })
+
+        .done(function(users) {
+          // $(".chat-group-user").empty();
+          if(users.length !== 0) {
+            users.forEach(function(users){
+              appendUsers(users);
+            });
+          }
+          else {
+            appendNoUsers("一致するユーザーはいません");
+          }
+        })
+        .fail(function(){
+          alert('ユーザー検索に失敗しました');
+        })
+      })
+
+  $(document).on("click", ".user-search-add", function() {
+    var user_id = $(this).attr("data-user-id");
+    var user_name = $(this).attr("data-user-name");
+    $(this).parent().remove();
+    addUser(user_id, user_name);
+  });
+
+  $(document).on("click", ".user-search-remove", function() {
+    $(this).parent().remove();
   });
 });
+
